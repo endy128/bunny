ab1af860586184641d6f6d39eafff24ea2be47a3
Neil Douglas <endy64@icloud.com>
2024-04-08 17:09:12 +0100

    use struct for player

--- src/main.c
+++ src/main.c
@@ -6,48 +6,64 @@
 #define ANIM_UP 2
 #define ANIM_SIDE 3
 
-Sprite *player;
-int player_x = 0;
-int player_y = 0;
+// Sprite *player;
+// int player.x = 0;
+// int player.y = 0;
 
-static void handleInput()
+struct Entity
+{
+    int x;
+    int y;
+    int w;
+    int h;
+    int velx;
+    int vely;
+    int health;
+    Sprite *sprite;
+    char name[6];
+};
+
+struct Entity player = {110, 0, 16, 16, 0, 0, 0, NULL, "PLAYER"};
+
+static void
+handleInput()
 {
     u16 value = JOY_readJoypad(JOY_1);
 
     if (value & BUTTON_RIGHT)
     {
-        player_x += 1;
+        player.x += 1;
     }
     else if (value & BUTTON_LEFT)
     {
-        player_x -= 1;
+        player.x -= 1;
     }
     if ((value & BUTTON_RIGHT) && !(value & BUTTON_UP) && !(value & BUTTON_DOWN))
     {
-        SPR_setAnim(player, ANIM_SIDE);
-        SPR_setHFlip(player, TRUE);
+        SPR_setAnim(player.sprite, ANIM_SIDE);
+        SPR_setHFlip(player.sprite, TRUE);
     }
     if ((value & BUTTON_LEFT) && !(value & BUTTON_UP) && !(value & BUTTON_DOWN))
     {
-        SPR_setAnim(player, ANIM_SIDE);
-        SPR_setHFlip(player, FALSE);
+        SPR_setAnim(player.sprite, ANIM_SIDE);
+        SPR_setHFlip(player.sprite, FALSE);
     }
     if (value & BUTTON_DOWN)
     {
-        player_y += 1;
-        SPR_setAnim(player, ANIM_DOWN);
+        player.y += 1;
+        SPR_setAnim(player.sprite, ANIM_DOWN);
     }
     else if (value & BUTTON_UP)
     {
-        player_y -= 1;
-        SPR_setAnim(player, ANIM_UP);
+        player.y -= 1;
+        SPR_setAnim(player.sprite, ANIM_UP);
     }
 
     if (!(value & BUTTON_DOWN) && !(value & BUTTON_UP) && !(value & BUTTON_LEFT) && !(value & BUTTON_RIGHT))
     {
-        SPR_setAnim(player, ANIM_IDLE);
+        SPR_setAnim(player.sprite, ANIM_IDLE);
     }
-    SPR_setPosition(player, player_x, player_y);
+    SPR_setPosition(player.sprite, player.x, player.y);
 }
 
 int main()
@@ -60,7 +76,7 @@ int main()
 
     SPR_init();
     PAL_setPalette(PAL2, rabbit.palette->data);
-    player = SPR_addSprite(&rabbit, player_x, player_y, TILE_ATTR(PAL2, FALSE, FALSE, TRUE));
+    player.sprite = SPR_addSprite(&rabbit, player.x, player.y, TILE_ATTR(PAL2, FALSE, FALSE, TRUE));
 
     while (1)
     {
