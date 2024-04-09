0b786087093b036ad790a37ca9addf3eb073881f
Neil Douglas <endy64@icloud.com>
2024-04-09 20:12:58 +0100

    added bullets, fire in directions and die if off screen

--- src/main.c
+++ src/main.c
@@ -6,13 +6,17 @@
 #define ANIM_UP 2
 #define ANIM_SIDE 3
 
-#define MAX_BULLETS 3
+#define MAX_BULLETS 12
+#define BULLET_SPEED 3
+
+const u16 MAX_SCREEN_HEIGHT = 240;
+const u16 MAX_SCREEN_WIDTH = 320;
 
 // Sprite *player;
 // int player.x = 0;
 // int player.y = 0;
 
-struct Entity
+typedef struct Entity
 {
     int x;
     int y;
@@ -23,20 +27,20 @@ struct Entity
     int health;
     Sprite *sprite;
     char name[6];
-};
+} entity;
 
-struct Entity player = {110, 0, 16, 16, 0, 0, 0, NULL, "PLAYER"};
+entity player = {110, 0, 16, 16, 0, 0, 0, NULL, "PLAYER"};
 
-struct Entity bullets[MAX_BULLETS];
+entity bullets[MAX_BULLETS];
 u16 bulletsOnScreen = 0;
 
-void killEntity(struct Entity *e)
+void killEntity(entity *e)
 {
     e->health = 0;
     SPR_setVisibility(e->sprite, HIDDEN);
 }
 
-void reviveEntity(struct Entity *e)
+void reviveEntity(entity *e)
 {
     e->health = 1;
     SPR_setVisibility(e->sprite, VISIBLE);
@@ -44,8 +48,7 @@ void reviveEntity(struct Entity *e)
 
 void setUpBullets()
 {
-
-    struct Entity *b = bullets;
+    entity *b = bullets;
     for (u16 i = 0; i < MAX_BULLETS; i++)
     {
         b->x = 0;
@@ -62,7 +65,7 @@ void shootBullet()
 {
     if (bulletsOnScreen < MAX_BULLETS)
     {
-        struct Entity *b;
+        entity *b;
         u16 i = 0;
         for (i = 0; i < MAX_BULLETS; i++)
         {
@@ -70,11 +73,12 @@ void shootBullet()
             if (b->health == 0)
             {
 
-                b->x = player.x + 4;
-                b->y = player.y;
+                b->x = player.x + 12; // half the player sprite width
+                b->y = player.y + 8;
 
                 reviveEntity(b);
-                b->vely = -3;
+                b->vely = player.vely * BULLET_SPEED;
+                b->velx = player.velx * BULLET_SPEED;
 
                 SPR_setPosition(b->sprite, b->x, b->y);
                 bulletsOnScreen++;
@@ -84,38 +88,93 @@ void shootBullet()
     }
 }
 
-static void
-handleInput()
+static bulletOffScreen(entity *b)
+{
+    if ((b->y + b->h < 0) || (b->y + b->h > MAX_SCREEN_HEIGHT))
+    {
+        killEntity(b);
+        bulletsOnScreen--;
+    }
+    else if ((b->x + b->h < 0) || (b->x + b->h > MAX_SCREEN_WIDTH))
+    {
+        killEntity(b);
+        bulletsOnScreen--;
+    }
+    else
+    {
+        SPR_setPosition(b->sprite, b->x, b->y);
+    }
+}
+
+static void positionBullet()
+{
+    entity *b;
+    for (u16 i = 0; i < MAX_BULLETS; i++)
+    {
+        b = &bullets[i];
+        if (b->health > 0)
+        {
+            b->y += b->vely;
+            b->x += b->velx;
+            bulletOffScreen(b);
+            // if (b->y + b->h < 0) // ONLY CHECKS TOP OF SCREEN
+            // {
+            //     killEntity(b);
+            //     bulletsOnScreen--;
+            // }
+            // else
+            // {
+            //     SPR_setPosition(b->sprite, b->x, b->y);
+            // }
+        }
+    }
+}
+
+// D-Pad
+static void handleInput()
 {
     u16 value = JOY_readJoypad(JOY_1);
 
     if (value & BUTTON_RIGHT)
     {
         player.x += 1;
+        player.velx = 1;
     }
     else if (value & BUTTON_LEFT)
     {
         player.x -= 1;
+        player.velx = -1;
     }
     if ((value & BUTTON_RIGHT) && !(value & BUTTON_UP) && !(value & BUTTON_DOWN))
     {
+        player.velx = 1;
+        player.vely = 0;
         SPR_setAnim(player.sprite, ANIM_SIDE);
         SPR_setHFlip(player.sprite, TRUE);
     }
     if ((value & BUTTON_LEFT) && !(value & BUTTON_UP) && !(value & BUTTON_DOWN))
     {
+        player.velx = -1;
+        player.vely = 0;
         SPR_setAnim(player.sprite, ANIM_SIDE);
         SPR_setHFlip(player.sprite, FALSE);
     }
     if (value & BUTTON_DOWN)
     {
         player.y += 1;
+        player.vely = 1;
         SPR_setAnim(player.sprite, ANIM_DOWN);
     }
     else if (value & BUTTON_UP)
     {
         player.y -= 1;
+        player.vely = -1;
         SPR_setAnim(player.sprite, ANIM_UP);
+    }
+
+    if (((value & BUTTON_DOWN) || (value & BUTTON_UP)) && !(value & BUTTON_RIGHT) && !(value & BUTTON_LEFT))
+    {
+        player.velx = 0;
     }
 
     if (!(value & BUTTON_DOWN) && !(value & BUTTON_UP) && !(value & BUTTON_LEFT) && !(value & BUTTON_RIGHT))
@@ -124,7 +183,53 @@ handleInput()
     }
     SPR_setPosition(player.sprite, player.x, player.y);
 
-    if (value & BUTTON_B)
+    // player.velx = 0;
+    // player.vely = 0;
+
+    // if (value & BUTTON_B)
+    // {
+    //     shootBullet();
+    // }
+}
+
+// handle button presses for non D-Pad
+static void joyEvent(u16 joy, u16 changed, u16 state)
+{
+    // if (changed & state & BUTTON_RIGHT)
+    // {
+    //     player.x += 1;
+    // }
+    // else if (changed & state & BUTTON_LEFT)
+    // {
+    //     player.x -= 1;
+    // }
+    // if ((changed & state & BUTTON_RIGHT) && !(changed & state & BUTTON_UP) && !(changed & state & BUTTON_DOWN))
+    // {
+    //     SPR_setAnim(player.sprite, ANIM_SIDE);
+    //     SPR_setHFlip(player.sprite, TRUE);
+    // }
+    // if ((changed & state & BUTTON_LEFT) && !(changed & state & BUTTON_UP) && !(changed & state & BUTTON_DOWN))
+    // {
+    //     SPR_setAnim(player.sprite, ANIM_SIDE);
+    //     SPR_setHFlip(player.sprite, FALSE);
+    // }
+    // if (changed & state & BUTTON_DOWN)
+    // {
+    //     player.y += 1;
+    //     SPR_setAnim(player.sprite, ANIM_DOWN);
+    // }
+    // else if (changed & state & BUTTON_UP)
+    // {
+    //     player.y -= 1;
+    //     SPR_setAnim(player.sprite, ANIM_UP);
+    // }
+
+    // if (!(changed & state & BUTTON_DOWN) && !(changed & state & BUTTON_UP) && !(changed & state & BUTTON_LEFT) && !(changed & state & BUTTON_RIGHT))
+    // {
+    //     SPR_setAnim(player.sprite, ANIM_IDLE);
+    // }
+
+    if (changed & state & BUTTON_B)
     {
         shootBullet();
     }
@@ -133,6 +238,8 @@ handleInput()
 int main()
 {
     VDP_drawText("Hello Neil!!", 14, 27);
+
+    JOY_setEventHandler(joyEvent);
 
     u16 ind = TILE_USERINDEX;
     PAL_setPalette(PAL0, bg1.palette->data);
@@ -150,6 +257,7 @@ int main()
         handleInput();
         SPR_update();
         SYS_doVBlankProcess();
+        positionBullet();
     }
     return (0);
 }
